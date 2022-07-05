c085bb75e3ebd4df705cb54e3b57fa6c68af4c1b
Tsutsui <mrsushieyt@gmail.com>
2022-07-05 02:38:10 +0000

    Added button test

--- index.js
+++ index.js
@@ -2,24 +2,57 @@
       Coded by Tsutsui#3661, Started by July 4th 20222, Almost my birthday. 
 */
 
-// Grabbing token from config.json
 const { token } = require('./config.json')
-
-// Getting required classes
-const { Client, Intents } = require('discord.js')
+const { Client, Intents, MessageEmbed } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-// If the bot is online, it will log that it's ready in the Console / Terminal
+const { MessageActionRow, MessageButton } = require('discord.js');
+
 client.on('ready', (c) => {
       console.log(`Bot is ready! As: ${c.user.tag}`)
 })
 
-// Message Event
-client.on('messageCreate', (message) => {
-      if (message.content.toLowerCase().startsWith("-ping")) {
-            message.reply("**Pong!**")
+let prefix = "-"
+
+client.on('messageCreate', async (message) => {
+      if (message.author.bot) return
+
+      if (message.content.toLowerCase().startsWith(prefix + 'ping')) {
+            const msg = message.content.split(' ').slice(1).join(' ')
+            
+            if (!msg) {
+                  message.reply("**Pong!**")
+            } else message.reply(`**An error has occurred, cannot execute *\"${msg}\"***`)
       }
+
+      if (message.content.toLowerCase().startsWith(prefix + 'button')) {
+            const filter = i => i.customId === 'primary' && i.user.id === message.author.id;
+            const collector = message.channel.createMessageComponentCollector({ filter, time: 1000 * 15 });
+            const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Press here!')
+					.setStyle('SUCCESS'),
+			);
+            const embed = new MessageEmbed().setColor("GREEN").setTitle("Testing Button Feature!")
+
+            collector.on('collect', async i => {
+                  if (i.customId === 'primary') {
+                        await i.update({ embeds: [embed.setTitle("Button has been clicked, thanks!")], components: [] });
+                        collector.stop()
+                  }
+            });
+
+            collector.on('end', collected => {
+                  console.log(`Finished collecting, collected ${collected.size} item`)
+            })
+
+            await message.reply({ embeds: [embed], components: [row] });
+      }
+
+      
+
 })
 
-// Login into the bot with its token
 client.login(token)
